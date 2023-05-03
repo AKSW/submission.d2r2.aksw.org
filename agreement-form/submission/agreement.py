c27c49430fbedebfaee0d02ce4050d4a071cc550
Natanael Arndt <natanael.arndt@eccenca.com>
2023-05-03 15:05:04 +0200

    Add click command to fill form

--- agreement-form/submission/agreement.py
+++ agreement-form/submission/agreement.py
@@ -4,6 +4,19 @@ import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
+import yaml
+import click
+
+LINE_LIMIT = 76
+
+
+def split_line(line):
+    if len(line) > LINE_LIMIT:
+        split_pos = line[:LINE_LIMIT].rfind(' ')
+        return line[:split_pos], line[split_pos + 1:]
+    return line, ""
+
+
 def fill_text(agreement_pdf, output, data):
 
     corresponding_author = data["corresponding_author"]
@@ -12,17 +25,20 @@ def fill_text(agreement_pdf, output, data):
     form_text = io.BytesIO()
     can = canvas.Canvas(form_text, pagesize=A4)
     can.setFont('Courier', 12)
-    can.drawString(20, 710, data["title"])
-    can.drawString(20, 695, data["title_line2"])
-    can.drawString(20, 650, data["authors"])
-    can.drawString(70, 585, corresponding_author["name"])
-    can.drawString(70, 563, corresponding_author["affiliation"])
-    can.drawString(70, 539, corresponding_author["address"])
-    can.drawString(70, 518, corresponding_author["email"])
-    can.drawString(20, 420, event["title"])
-    can.drawString(20, 405, event["title_line2"])
-    can.drawString(20, 375, event["editors"])
-    can.drawString(20, 360, event["editors_line2"])
+    can.drawString(20, 710, split_line(data["title"])[0])
+    can.drawString(20, 695, split_line(data["title"])[1])
+    can.drawString(20, 650, split_line(data["authors"])[0])
+    can.drawString(20, 635, split_line(data["authors"])[1])
+    can.drawString(70, 586, corresponding_author["name"])
+    can.drawString(70, 563, split_line(corresponding_author["affiliation"])[0])
+    can.drawString(70, 553, split_line(corresponding_author["affiliation"])[1])
+    can.drawString(70, 540, split_line(corresponding_author["address"])[0])
+    can.drawString(70, 530, split_line(corresponding_author["address"])[1])
+    can.drawString(70, 517, corresponding_author["email"])
+    can.drawString(20, 420, split_line(event["title"])[0])
+    can.drawString(20, 405, split_line(event["title"])[1])
+    can.drawString(20, 375, split_line(event["editors"])[0])
+    can.drawString(20, 360, split_line(event["editors"])[1])
     can.save()
 
     form_text.seek(0)
@@ -41,6 +57,7 @@ def fill_text(agreement_pdf, output, data):
     with open(output, 'wb') as out:
         pdf_writer.write(out)
 
+
 def wrap_title(title):
     title_line2 = ""
     if (len(title) > 96):
@@ -48,28 +65,24 @@ def wrap_title(title):
         pass
     return title, title_line2
 
+
+@click.command()
+@click.option("--metadata", default="metadata.yml", help="The metadata.yml file.")
+@click.option("--template", default="ceur-author-agreement-ccby-ntp.pdf", help="The template pdf file.")
+@click.option("--output", default="agreement_filled.pdf", help="The filled pdf file.")
+def fill_form(metadata, template, output):
+    """Fill the form with the metadata provided."""
+    with open(metadata, "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+            print(data)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    for paper in data.values():
+        paper["authors"] = ", ".join([author["name"] for author in paper["authors"]])
+        fill_text(agreement_pdf=template, output=output, data=paper)
+
+
 if __name__ == '__main__':
-    LINE_LIMIT = 76
-
-    data = {
-        "title": "Title Line 1",
-        "title_line2": "Title Line 2",
-        "authors": "All Authors",
-        "corresponding_author": {
-            "name": "Corresponding Author",
-            "affiliation": "Affiliation of Corresponding Author",
-            "address": "Address of Affiliation",
-            "email": "email of Corresponding Author"
-        },
-        "event": {
-            "title": "Second International Workshop on Linked Data-driven Resilience Research 2023",
-            "title_line2": "",
-            "editors": "Ricardo Usbeck, Sören Auer, Julia Holze, Sebastian Tramp, and Natanael Arndt",
-            "editors_line2": ""
-        }
-    }
-
-    fill_text(
-        agreement_pdf='ceur-author-agreement-ccby-ntp.pdf',
-        output='agreement_filled.pdf',
-        data=data)
+    fill_form()
